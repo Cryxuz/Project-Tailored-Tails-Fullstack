b8eb7555d595e4197f2dc37602460ec3c6a21ffc
Paul David Tan <cryxuz@gmail.com>
2023-10-30 15:51:59 +1300

    read del create

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,34 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// GET request
+
+app.get('/decks', async (req: Request, res: Response) => {
+  const decks = await Deck.find()
+  console.log(decks)
+  res.json(decks)
+ } )
+ 
+ // POST request.
+ 
+ app.post('/decks', async (req: Request, res: Response) => {
+   console.log(req.body)
+   // make a newDeck  for the database.
+   const newDeck = new Deck({
+     title: req.body.title,
+   });
+   // saving the newDeck to the database.
+   const createdDeck = await newDeck.save()
+   res.json(createdDeck)
+ })
+ 
+ // delete 
+ app.delete('/decks/:deckId', async (req: Request, res: Response) => {
+   const deckId = req.params.deckId
+   const deck = await Deck.findByIdAndDelete(deckId)
+   res.json(deck)
+ })
+
 mongoose
 .connect(process.env.MONGO_URL!)
 .then(() => {

0ca25a4361664f027ca46ce11a4546886219d3a4
Paul David Tan <cryxuz@gmail.com>
2023-10-31 14:19:02 +1300

    home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,8 +4,14 @@ function Home () {
 return (
   <div>
   <div className="grid grid-cols-2 justify-center items-center">
-  <h1 className="m-auto text-8xl">Tailored Tails</h1>
-    <img className="p-8 rounded-md" src="/images/bg-home.jpg" alt="" />
+    <div>
+    <h1 className="m-auto text-8xl">Tailored Tails</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio atque natus minus inventore, unde at dolorum consequatur non corporis quia dolor optio saepe laborum, expedita sequi distinctio culpa, adipisci nemo.</p>
+    </div>
+    
+    <img className="p-8 rounded-lg" src="/images/bg-home.jpg" alt="" />
+    
+    
   </div>
   <Link className='flex justify-center' to="/items">
   <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Check Out Our Products</button>

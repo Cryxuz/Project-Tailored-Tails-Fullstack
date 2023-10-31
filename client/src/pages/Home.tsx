6885255274d86c019a68fc506079088347141b3d
Paul David Tan <cryxuz@gmail.com>
2023-10-31 14:12:52 +1300

    pages

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,20 @@
+import { Link } from 'react-router-dom'
+
 function Home () {
 return (
-  <>
-  <h1>Tailored Tails</h1>
   <div>
-    <img src="/images/bg-home.jpg" alt="" />
+  <div className="grid grid-cols-2 justify-center items-center">
+  <h1 className="m-auto text-8xl">Tailored Tails</h1>
+    <img className="p-8 rounded-md" src="/images/bg-home.jpg" alt="" />
   </div>
-  </>
+  <Link className='flex justify-center' to="/items">
+  <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Check Out Our Products</button>
+  </Link>
+  <div>
+  <h1 className='text-center'>insert carousel here</h1>
+  {/* carousel */}
+  </div>
+  </div>
 )
 }
 

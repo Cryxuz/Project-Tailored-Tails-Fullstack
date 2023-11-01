65d867fe843c22f1043a2daf14cbca947fba37ad
Kadin Vo <kadinvo@gmail.com>
2023-11-02 11:41:15 +1300

    Changed carousel to rotate 4 pictures

--- client/src/components/Carousel.tsx
+++ client/src/components/Carousel.tsx
@@ -1,55 +1,47 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { ItemInterface } from '../interfaces/iteminterface'
 
 const Carousel: React.FC = () => {
   const [images, setImages] = useState<ItemInterface[]>([])
-  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0)
+  const [visibleImages, setVisibleImages] = useState<ItemInterface[]>([])
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
 
   useEffect(() => {
-    let isMounted = true
-
     axios
       .get<ItemInterface[]>('http://localhost:3000/items')
       .then((response) => {
-        if (isMounted) {
-          setImages(response.data)
-        }
+        setImages(response.data)
+        setVisibleImages(response.data.slice(0, 4))
       })
       .catch((error) => {
         console.error('Error fetching items:', error)
       })
-
-    return () => {
-      isMounted = false
-    }
   }, [])
 
   useEffect(() => {
-    const nextImage = () => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length)
-    }
-
     const timer = setInterval(() => {
-      nextImage()
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % (images.length - 3))
     }, 5000)
 
     return () => {
       clearInterval(timer)
     }
-  }, [currentImageIndex, images])
+  }, [currentIndex, images])
+
+  useEffect(() => {
+    setVisibleImages(images.slice(currentIndex, currentIndex + 4))
+  }, [currentIndex, images])
 
   return (
     <div className="w-full h-80 overflow-hidden relative">
-      <div className="flex transition-transform duration-300 ease-in-out">
-        {images.map((image, index) => (
+      <div className="flex transition-transform duration-500 ease-in-out">
+        {visibleImages.map((image) => (
           <img
             key={image._id}
             src={image.imageUrl}
             alt={image.name}
-            className={`w-full h-full absolute transform ${
-              index === currentImageIndex ? 'translate-x-0' : 'translate-x-full'
-            } transition-transform duration-500`}
+            className="w-1/4 h-full object-cover ml-2 mr-2"
           />
         ))}
       </div>

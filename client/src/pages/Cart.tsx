b36af9236a62aeef89b792d1950f86bfe281042b
Paul David Tan <cryxuz@gmail.com>
2023-11-02 15:20:11 +1300

    logo, footer margin, nav padding

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -1,4 +1,26 @@
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import { ItemInterface } from "../interfaces/iteminterface";
+// import { useParams } from "react-router-dom";
+
 const Cart = () => {
+  // const { itemId } = useParams()
+  // const [item, setItem] = useState<ItemInterface | null>(null);
+  // useEffect(() => {
+  //   axios
+  //     .get<ItemInterface>(`http://localhost:3000/items/${itemId}`)
+  //     .then((response) => {
+  //       console.log(response.data);
+  //       setItem(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error fetching item:', error);
+  //     });
+  // }, [itemId]);
+  // if (item === null) {
+  //   return <div>Loading...</div>;
+  // }
+  // console.log
   return (
   <div>
     <div className="md:grid md:grid-cols-4 gap-[2%] p-10">
@@ -17,19 +39,25 @@ const Cart = () => {
         <p>Rural delivery available</p>
         <p>International delivery available</p>
         <br />
-        <h3 className="text-lg font-bold">Payment Options</h3>
-        <p>Visa, Mastercard & Amex</p>
+        <h3 className="text-lg font-bold py-[5%]">Payment Options</h3>
+        <div >
+        <p><img src="/images/card.png" alt="master-card logo" /><img src="images/visa.png" alt="visa logo" /><img src="/images/paypal-logo.png" alt="paypal logo" /></p>
+        </div>
         <p>Internet Banking</p>
         <p>POLi</p>
         <p>Zip</p>
         <p>Afterpay</p>
-        <p>PayPal</p>
         <p>Online EFTPOS</p>
         <p>Payment on collection from Fake Adress, Auckland</p>
         <button className="p-2 bg-orange-600 hover:bg-orange-500 rounded-lg text-white hidden md:block my-[3%]">Checkout</button>
-      </div>
-      
+      </div> 
     </div>
+    {/* Suggestions
+    <div>
+    <p>Suggestions from our best sellers</p>
+    <img
+        src={item.imageUrl}/>
+    </div> */}
   </div>
   )
 }

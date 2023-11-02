c35a367c0963d0d65cdc4baf789bbb269d59aaa3
Paul David Tan <cryxuz@gmail.com>
2023-11-02 15:56:41 +1300

    added suggestions cart

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -1,65 +1,136 @@
-// import axios from "axios";
-// import { useEffect, useState } from "react";
-// import { ItemInterface } from "../interfaces/iteminterface";
-// import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import { ItemInterface } from "../interfaces/iteminterface";
 
 const Cart = () => {
-  // const { itemId } = useParams()
-  // const [item, setItem] = useState<ItemInterface | null>(null);
-  // useEffect(() => {
-  //   axios
-  //     .get<ItemInterface>(`http://localhost:3000/items/${itemId}`)
-  //     .then((response) => {
-  //       console.log(response.data);
-  //       setItem(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.error('Error fetching item:', error);
-  //     });
-  // }, [itemId]);
-  // if (item === null) {
-  //   return <div>Loading...</div>;
-  // }
-  // console.log
-  return (
-  <div>
-    <div className="md:grid md:grid-cols-4 gap-[2%] p-10">
-      <div className="col-span-3 bg-sky-300">
-        <h1>Delete divs when working with cart</h1>
-        <div className="w-[100px] h-[100px] bg-slate-300">div</div>
-        <div className="w-[100px] h-[100px] bg-slate-300">div</div>
-        <div className="w-[100px] h-[100px] bg-slate-300">div</div>
-        <div className="w-[100px] h-[100px] bg-slate-300">div</div>
-      </div>
-      <button className="p-2 bg-orange-600 hover:bg-orange-500 rounded-lg text-white md:hidden my-[3%]">Checkout</button>
-      <div>
-        <h3 className="text-lg font-bold">Delivery Options</h3>
-        <p>NZ-wide from $5.95</p>
-        <p>Same day delivery for $9.99</p>
-        <p>Rural delivery available</p>
-        <p>International delivery available</p>
-        <br />
-        <h3 className="text-lg font-bold py-[5%]">Payment Options</h3>
-        <div >
-        <p><img src="/images/card.png" alt="master-card logo" /><img src="images/visa.png" alt="visa logo" /><img src="/images/paypal-logo.png" alt="paypal logo" /></p>
-        </div>
-        <p>Internet Banking</p>
-        <p>POLi</p>
-        <p>Zip</p>
-        <p>Afterpay</p>
-        <p>Online EFTPOS</p>
-        <p>Payment on collection from Fake Adress, Auckland</p>
-        <button className="p-2 bg-orange-600 hover:bg-orange-500 rounded-lg text-white hidden md:block my-[3%]">Checkout</button>
-      </div> 
-    </div>
-    {/* Suggestions
-    <div>
-    <p>Suggestions from our best sellers</p>
-    <img
-        src={item.imageUrl}/>
-    </div> */}
-  </div>
-  )
-}
+  const { itemId } = useParams();
+  const [items, setItems] = useState<ItemInterface[] | null>(null);
 
-export default Cart
+  useEffect(() => {
+    axios
+      .get<ItemInterface[]>(`http://localhost:3000/cart`)
+      .then((response) => {
+        console.log(response.data);
+        setItems(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching items:', error);
+      });
+  }, [itemId]);
+
+  if (items === null) {
+    return <div>Loading...</div>;
+  }
+
+  
+  const filteredItems = items.filter((item, index) => [1, 2, 0, 3].includes(index));
+
+  return (
+    <div>
+      <div className="md:grid md:grid-cols-4 gap-[2%] p-10">
+        <div className="col-span-3 bg-sky-300">
+          <h1>Delete divs when working with cart</h1>
+          <div className="w-[100px] h-[100px] bg-slate-300">div</div>
+          <div className="w-[100px] h-[100px] bg-slate-300">div</div>
+          <div className="w-[100px] h-[100px] bg-slate-300">div</div>
+          <div className="w-[100px] h-[100px] bg-slate-300">div</div>
+        </div>
+        <button className="p-2 bg-orange-600 hover:bg-orange-500 rounded-lg text-white md:hidden my-[3%]">
+          Checkout
+        </button>
+        <div>
+          <h3 className="text-lg font-bold">Delivery Options</h3>
+          <p>NZ-wide from $5.95</p>
+          <p>Same day delivery for $9.99</p>
+          <p>Rural delivery available</p>
+          <p>International delivery available</p>
+          <br />
+          <h3 className="text-lg font-bold py-[5%]">Payment Options</h3>
+          <div>
+            <p>
+              <img src="/images/card.png" alt="master-card logo" />
+              <img src="images/visa.png" alt="visa logo" />
+              <img src="/images/paypal-logo.png" alt="paypal logo" />
+            </p>
+          </div>
+          <p>Internet Banking</p>
+          <p>POLi</p>
+          <p>Zip</p>
+          <p>Afterpay</p>
+          <p>Online EFTPOS</p>
+          <p>Payment on collection from Fake Adress, Auckland</p>
+          <button className="p-2 bg-orange-600 hover:bg-orange-500 rounded-lg text-white hidden md:block my-[3%]">
+            Checkout
+          </button>
+        </div>
+      </div>
+
+      {/* Suggestions */}
+      
+        <p className="text-center text-2xl font-bold">Suggestions from our best sellers</p>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6 p-4 md:p-16">
+          {filteredItems.map((item) => (
+          <div className="text-xl font-semibold">
+            {item.name}
+            <img key={item._id} src={item.imageUrl} alt={item.name} />
+            <p>
+                <span className="font-medium text-lg">Category: </span>{' '}
+                {item.category}
+              </p>
+              <p>
+                <span className="font-medium text-lg">Price:</span> $
+                {item.price}
+              </p>
+              <p>
+                <span className="font-medium text-lg">Rating:</span>{' '}
+                {(() => {
+                  switch (item.rating) {
+                    case 1:
+                      return '★☆☆☆☆'
+                    case 2:
+                      return '★★☆☆☆'
+                    case 3:
+                      return '★★★☆☆'
+                    case 4:
+                      return '★★★★☆'
+                    case 5:
+                      return '★★★★★'
+                    default:
+                      return 'Not rated'
+                  }
+                })()}
+              </p>
+              <p>
+                
+                <span
+                  className={
+                    item.stock === 0 ? 'text-red-500' : 'text-green-700'
+                  }
+                >
+                  {item.stock === 0 ? 'Out of Stock' : 'In Stock'}
+                </span>
+              </p>
+              {item.stock > 0 ? (
+                <div className="flex gap-2 flex-end">
+                  <button className="p-2 bg-orange-600 rounded-lg text-white hover:bg-orange-500 mt-4">
+                    Add To Cart
+                  </button>
+                  
+                </div>
+              ) : (
+                <div className="flex  gap-2">
+                  <div className="p-2 bg-red-600 rounded-lg text-white mt-4">
+                    Out of Stock
+                  </div>       
+                </div>
+              )}
+          </div>
+        ))}
+        </div>
+      </div>
+    
+  );
+};
+
+export default Cart;

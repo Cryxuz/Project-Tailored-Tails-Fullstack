b1ac1011327af146ab7749a28785dac2594023ba
Paul David Tan <cryxuz@gmail.com>
2023-11-02 15:59:58 +1300

    deleted unecessary imports

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { Link, useParams } from "react-router-dom";
+import {  useParams } from "react-router-dom";
 import { ItemInterface } from "../interfaces/iteminterface";
 
 const Cart = () => {

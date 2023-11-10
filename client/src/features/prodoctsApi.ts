62d7c851eccc2f13e0f087c57f7d81c2aca0d153
Kadin Vo <kadinvo@gmail.com>
2023-11-10 16:25:12 +1300

    adjusted api

--- client/src/features/prodoctsApi.ts
+++ client/src/features/prodoctsApi.ts
@@ -1,13 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-export const productsApi = createApi({
-  reducerPath: 'productsApi',
+export const itemsApi = createApi({
+  reducerPath: 'itemsApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
-      query: () => 'products',
+      query: () => 'items',
     }),
   }),
 })
 
-export const { useGetAllProductsQuery } = productsApi
+export const { useGetAllProductsQuery } = itemsApi

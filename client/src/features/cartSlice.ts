b2ae3312cf25afb737f909e2b0f6d9256d373e13
Kadin Vo <kadinvo@gmail.com>
2023-11-12 18:50:26 +1300

    Added remove from cart logic

--- client/src/features/cartSlice.ts
+++ client/src/features/cartSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-import {toast} from "react-toastify"
+import { toast } from 'react-toastify'
 interface CartItems {
   name: string
   id: string
@@ -16,39 +16,47 @@ interface Cart {
 const initialState: Cart = {
   // the line below will include the localStorage cartItems to the application state.
   // cartItems inside getItem is the key from localStorage.setItem('cartItems') below
-  cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')): [],
+  cartItems: localStorage.getItem('cartItems')
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    : [],
   cartTotalQuantity: 0,
   cartTotalAmount: 0,
 }
-      
+
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
     addToCart(state, action) {
-
-    
       // if we have product in the cart we increase its quantity
-    
-      const itemIndex =  state.cartItems.findIndex( (item) => item.name === action.payload.name)
-      if(itemIndex >= 0) {
-        state.cartItems[itemIndex].cartQuantity += 1;
+
+      const itemIndex = state.cartItems.findIndex(
+        (item) => item.name === action.payload.name
+      )
+      if (itemIndex >= 0) {
+        state.cartItems[itemIndex].cartQuantity += 1
         toast.info(`increased ${state.cartItems[itemIndex].name} quantity`, {
-          position: "bottom-left"
+          position: 'bottom-left',
         })
       } else {
         // if we dont have product in the cart we run this code
-      const tempProduct = {...action.payload, cartQuantity: 1};
-      state.cartItems.push(tempProduct)
-      toast.success(`${action.payload.name} added to cart`, {
-        position: "bottom-left"
-      })
+        const tempProduct = { ...action.payload, cartQuantity: 1 }
+        state.cartItems.push(tempProduct)
+        toast.success(`${action.payload.name} added to cart`, {
+          position: 'bottom-left',
+        })
       }
       // cartItems is the key. this will add the cartItems to the localStorage and cartItems is the key
-      localStorage.setItem("cartItems", JSON.stringify(state.cartItems))
+      localStorage.setItem('cartItems', JSON.stringify(state.cartItems))
+    },
+    removeFromCart(state, action) {
+      const nextCartItems = state.cartItems.filter(
+        (cartItem) => cartItem.id !== action.payload.id
+      )
+      state.cartItems = nextCartItems
     },
   },
 })
 
-export const { addToCart } = cartSlice.actions
+export const { addToCart, removeFromCart } = cartSlice.actions
 export default cartSlice.reducer

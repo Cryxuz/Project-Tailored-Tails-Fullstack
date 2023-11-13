59ff1a1932a383676ba9c6a7212a1c0900cbd881
Paul David Tan <cryxuz@gmail.com>
2023-11-13 18:01:36 +1300

    register page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,14 +5,17 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
-import productsReducer, { productsFetch } from './features/productsSlice.js'
 import { itemsApi } from './features/productsApi.ts'
 import cartReducer, { getTotals } from './features/cartSlice.ts'
+import productsReducer, { productsFetch } from './features/productsSlice.js'
+import authReducer from './features/authSlice.ts'
+
 
 const store = configureStore({
   reducer: {
     products: productsReducer,
     cart: cartReducer,
+    auth: authReducer,
     [itemsApi.reducerPath]: itemsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,70 +1,26 @@
-import { useState } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
-import { UserErrors } from '../../../server/routes/errors'
+import { useState } from "react"
 
+const Register = () => {
 
-const RegisterPage = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const navigate = useNavigate()
-  const handleRegister = async () => {
-    try {
-      const response = await axios.post('http://localhost:3000/user/register', {
-        username,
-        password,
-      })
-      alert('Registration Completed! You may now login.')
-      console.log('User registered successfully:', response.data)
-      navigate('/login')
-    } catch (err) {
-      if (err.response.data.type === UserErrors.USERNAME_ALREADY_EXISTS) {
-        alert('ERROR: Username already in use')
-        console.error('Error registering user:', err)
-      } else {
-        alert('ERROR: Something went wrong')
-      }
-    }
-  }
-
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: ""
+  })
   return (
-    <div className="h-full w-full flex justify-center items-center mt-10 ">
-      <div className="p-8 rounded-lg shadow-md bg-slate-200">
-        <h2 className="text-2xl font-semibold mb-4">Register</h2>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Username:
-          </label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Enter Username"
-            className="w-full border border-gray-300 rounded py-2 px-3"
-          />
-        </div>
-        <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Password:
-          </label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter Password"
-            className="w-full border border-gray-400 rounded py-2 px-3"
-          />
-        </div>
+  <form action="">
+    <h2>Register</h2>
+    <label htmlFor="name">Name:</label>
+    <input type="text" placeholder="Enter Name" onChange={(e) => setUser({ ...user, name: e.target.value})}/>
 
-        <button
-          onClick={handleRegister}
-          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
-        >
-          Register
-        </button>
-      </div>
-    </div>
+    <label htmlFor="email">Email:</label>
+    <input type="email" placeholder="Enter Email" onChange={(e) => setUser({ ...user, name: e.target.value})}/>
+
+    <label htmlFor="password">Password:</label>
+    <input type="password" placeholder="Enter Password" onChange={(e) => setUser({ ...user, name: e.target.value})}/>
+    <button>Register</button>
+  </form>
   )
 }
 
-export default RegisterPage
+export default Register

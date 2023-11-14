3f4ad3d57546eea7ef6438c564a5690387da6931
Paul David Tan <cryxuz@gmail.com>
2023-11-14 14:55:36 +1300

    stripe total working

--- server/routes/stripe.ts
+++ server/routes/stripe.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import Stripe from 'stripe'
 import * as dotenv from 'dotenv'
-// const Stripe = require('stripe')
 dotenv.config()
 
 const stripeKey = process.env.STRIPE_KEY
@@ -15,19 +14,25 @@ const router = express.Router()
 const YOUR_DOMAIN = 'http://localhost:5173'
 
 router.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: 'Coat',
-          },
-          unit_amount: 1000,
+  const line_items = req.body.cartItems.map((item: any) => {
+    return {
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: item.name,
+          images: [item.imageUrl],
+          description: item.description,
+          metadata: {
+            id: item.id
+          }
         },
-        quantity: 1,
+        unit_amount: item.price * 100,
       },
-    ],
+      quantity: item.cartQuantity,
+    }
+  })
+  const session = await stripe.checkout.sessions.create({
+    line_items,
     mode: 'payment',
     success_url: `${YOUR_DOMAIN}/success`,
     cancel_url: `${YOUR_DOMAIN}/cart`,

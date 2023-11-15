cf1c999b792a926b85c887cdc16b5b72d00f725d
Kadin Vo <kadinvo@gmail.com>
2023-11-15 20:34:08 +1300

    Adjust

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,111 +1,174 @@
-import { Link } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { logoutUser } from '../features/authSlice';
-import { toast } from 'react-toastify';
+import { Link } from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { logoutUser } from '../features/authSlice'
+import { toast } from 'react-toastify'
 
 const Navbar = () => {
-  const dispatch = useDispatch();
-  const [open, setOpen] = useState(false);
-  const { cartTotalQuantity } = useSelector((state) => state.cart);
-  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch()
+  const [open, setOpen] = useState(false)
+  const { cartTotalQuantity } = useSelector((state) => state.cart)
+  const auth = useSelector((state) => state.auth)
 
   const handleMenu = () => {
-    setOpen((prev) => !prev);
-  };
+    setOpen((prev) => !prev)
+  }
 
   return (
-    <nav className="flex items-center justify-between shadow-lg h-20 px-[3%] lg:py-[3%] py-[7%]">
-      <p className='text-5xl cursive'><img className="inline pr-5" src="/images/logo.png" alt="paw print icon" />Tailored Tails</p>
-
-      {/* Hamburger menu for smaller screens */}
-      <div className="lg:hidden">
-        {open ? (
-        <div className='mt-[215px] bg-slate-200 rounded-lg p-4'>
-          <FaTimes className="text-3xl cursor-pointer" onClick={handleMenu} />
-          <li className='py-2 list-none'>
-            <Link to="/" className="px-3 py-2 font-semibold text-lg hover:underline">
-              Home
-            </Link>
-          </li>
-          <li className='py-2 list-none'>
-            <Link to="/items" className="px-3 py-2 font-semibold text-lg hover:underline">
-              Items
-            </Link>
-          </li>
-          <li className='py-2 list-none'>
-            <Link to="/cart" className="px-3 py-2 font-semibold text-lg hover:underline">
-            Cart<span className='bg-slate-600 rounded-full px-2 py-[1px] m-2 text-white'>{cartTotalQuantity}</span> 
-            </Link>
-          </li>
-          {!auth.name && (
-            <>
-              <li className="px-3 py-2 font-semibold text-lg hover:underline list-none">
-                <Link to="/registration">Register</Link>
-              </li>
-              <li className="px-3 py-2 font-semibold text-lg hover:underline list-none">
-                <Link to="/login">Login</Link>
-              </li>
-            </>
-          )}
-          {auth.name && (
-            <li className="px-3 py-2 font-semibold text-lg hover:underline list-none">
-              <Link to="/" onClick={() => {
-                dispatch(logoutUser(null));
-                toast.warning("You have logged out", { position: "bottom-left" });
-              }}>Logout</Link>
-            </li>
-          )}
+    <nav className="py-[1%] border-b shadow-md px-[3%]">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-black text-4xl font-semibold cursive hidden md:block">
+          <img className="inline pr-5" src="/images/logo.png" alt="" />
+          Tailored Tails
         </div>
-        )
-        
-        : 
-        
-        (
-          <FaBars className="text-3xl cursor-pointer" onClick={handleMenu} />
-        )}
-      </div>
-      {/* Navigation links */}
-      <ul className={`lg:flex text-2xl items-center space-x-4 justify-end semi-bold;
-      ${open ? 'hidden' : 'hidden'}`}>
-        <li>
-          <Link to="/" className="px-3 py-2 font-semibold hover:underline">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/items" className="px-3 py-2 font-semibold hover:underline">
-            Items
-          </Link>
-        </li>
-        <li>
-          <Link to="/cart" className="px-3 py-2 font-semibold hover:underline">
-          Cart<span className='bg-slate-600 rounded-full px-2 py-[1px] m-2 text-white'>
-            {cartTotalQuantity}</span> 
-          </Link>
-        </li>
-        {!auth.name && (
-          <>
-            <li className="px-3 py-2 font-semibold hover:underline">
-              <Link to="/registration">Register</Link>
-            </li>
-            <li className="px-3 py-2 font-semibold hover:underline">
-              <Link to="/login">Login</Link>
-            </li>
-          </>
-        )}
-        {auth.name && (
-          <li className="px-3 py-2 font-semibold hover:underline">
-            <Link to="/" onClick={() => {
-              dispatch(logoutUser(null));
-              toast.warning("You have logged out", { position: "bottom-left" });
-            }}>Logout</Link>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
-};
+        <div className="space-x-10 hidden md:block">
+          {auth.name ? (
+            <div>
+              <Link
+                to="/"
+                className="px-[5%] text-black font-semibold hover:underline merriweather text-xl"
+              >
+                Home
+              </Link>
+              <Link
+                to="/items"
+                className="px-[5%] text-black font-semibold hover:underline merriweather text-xl"
+              >
+                Items
+              </Link>
+              <Link
+                to="/category"
+                className="px-[5%] text-black font-semibold hover:underline merriweather text-xl"
+              >
+                Category
+              </Link>
+              <Link
+                to="/cart"
+                className="px-[5%] text-black font-semibold hover:underline merriweather text-xl"
+              >
+                Cart
+                <span className="bg-slate-600 rounded-full px-2 py-[1px] m-2 text-white">
+                  {cartTotalQuantity}
+                </span>
+              </Link>
+              <Link
+                to="/"
+                onClick={() => {
+                  dispatch(logoutUser(null))
+                  toast.warning('You have logged out', {
+                    position: 'bottom-left',
+                  })
+                }}
+                className="px-[5%] text-black font-semibold hover:underline merriweather text-xl"
+              >
+                Logout
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <Link
+                to="/"
+                className="px-[5%] text-black font-semibold hover:underline merriweather text-xl"
+              >
+                Home
+              </Link>
+              <Link
+                to="/items"
+                className="px-[5%] text-black font-semibold hover:underline merriweather text-xl"
+              >
+                Items
+              </Link>
+              <Link
+                to="/category"
+                className="px-[5%] text-black font-semibold hover:underline merriweather text-xl"
+              >
+                Category
+              </Link>
+              <Link
+                to="/cart"
+                className="px-[5%] text-black font-semibold hover:underline merriweather text-xl"
+              >
+                Cart
+                <span className="bg-slate-600 rounded-full px-2 py-[1px] m-2 text-white">
+                  {cartTotalQuantity}
+                </span>
+              </Link>
+              <Link
+                className="px-[5%] text-black font-semibold hover:underline merriweather text-xl"
+                to="/login"
+              >
+                Login
+              </Link>
+              <Link
+                className="px-[5%] text-black font-semibold hover:underline merriweather text-xl"
+                to="/registration"
+              >
+                Register
+              </Link>
+            </div>
+          )}
 
-export default Navbar;
+          {/* OLD LINKS */}
+          {/* <Link
+              to="/registration"
+              className="text-black font-semibold hover:underline merriweather text-xl"
+            >
+              Registration
+            </Link>
+
+            <Link
+              to="/login"
+              className="text-black font-semibold hover:underline merriweather text-xl"
+            >
+              Login
+            </Link> */}
+          {/* </div> */}
+          {/* Hamburger menu */}
+          <div className="-mr-2 flex border-2 border-black rounded-lg md:hidden">
+            <button
+              type="button"
+              onClick={handleMenu}
+              className="inline-flex items-center justify-center p-2 rounded-md text-black hover:text-black hover:bg-gray-500 hover:bg-opacity-30 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+            >
+              <span className="sr-only">Open Main Menu</span>
+              {open ? <FaTimes /> : <FaBars />}
+            </button>
+          </div>
+        </div>
+        {open ? (
+          <div className="md:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 flex flex-col text-slate-800">
+              <Link
+                to="/"
+                className="text-black font-semibold hover:underline merriweather text-xl"
+              >
+                Home
+              </Link>
+              <Link
+                to="/items"
+                className="text-black font-semibold hover:underline merriweather text-xl"
+              >
+                Items
+              </Link>
+              <Link
+                to="/category"
+                className="text-black font-semibold hover:underline merriweather text-xl"
+              >
+                Category
+              </Link>
+              <Link
+                to="/cart"
+                className="text-black font-semibold hover:underline merriweather text-xl"
+              >
+                Cart
+              </Link>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
